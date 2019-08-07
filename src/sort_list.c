06623c7f6a4440be7a102cb0165f5c042e223cca
HumanBean17 <terex220@icloud.com>
2019-08-07 12:54:31 +0300

    norme fixed

--- src/sort_list.c
+++ src/sort_list.c
@@ -28,21 +28,6 @@ int		cmp(const char *str1, const char *str2)
 	return (0);
 }
 
-void	print_level(t_subdir *level)
-{
-	t_subdir	*tmp;
-
-	tmp = NULL;
-	printf("-----------------\n");
-	while (level)
-	{
-		printf("%s---->%ld\n", level->name, level->mtime);
-		tmp = level;
-		level = level->next;
-	}
-	printf("-----------------\n");
-}
-
 void	swap_1(t_subdir **head, t_subdir **a, t_subdir **b)
 {
 	t_subdir	*first;
@@ -68,14 +53,56 @@ void	swap_1(t_subdir **head, t_subdir **a, t_subdir **b)
 		a_prev->next = (*b);
 }
 
-void	do_swap(t_subdir **head, t_subdir **a, t_subdir **b)
+void    prev_initil(t_subdir **a, t_subdir **b, t_subdir **a_prev, t_subdir **b_prev)
 {
-	t_subdir	*a_prev;
-	t_subdir	*b_prev;
-	t_subdir	*a_next;
-	t_subdir	*b_next;
-	int			flag_a;
-	int			flag_b;
+	t_subdir *tmp_a;
+	t_subdir *tmp_b;
+
+	tmp_a = *a_prev;
+	tmp_b = *b_prev;
+	while (tmp_a != (*a) && tmp_a && tmp_a->next != (*a))
+		tmp_a = tmp_a->next;
+	while (tmp_b != (*b) && tmp_b && tmp_b->next != (*b))
+		tmp_b = tmp_b->next;
+	*a_prev = tmp_a;
+	*b_prev = tmp_b;
+}
+
+int     a_flag_initil(t_subdir *a_prev, t_subdir **head, t_subdir **a)
+{
+	int flag_a;
+
+	flag_a = 0;
+	if (a_prev && a_prev == *head && a_prev->next != (*a))
+		flag_a = 1;
+	return (flag_a);
+}
+
+int     b_flag_initil(t_subdir *b_prev, t_subdir **head, t_subdir **b)
+{
+	int flag_b;
+
+	flag_b = 0;
+	if (b_prev && b_prev == *head && b_prev->next != (*b))
+		flag_b = 1;
+	return (flag_b);
+}
+
+void    a_head_condition(int flag_a, t_subdir **head, t_subdir **b, t_subdir **a_prev)
+{
+	if (flag_a == 0)
+		(*a_prev)->next = (*b);
+	else
+		(*head) = (*b);
+}
+
+void    do_swap(t_subdir **head, t_subdir **a, t_subdir **b)
+{
+	t_subdir *a_prev;
+	t_subdir *b_prev;
+	t_subdir *a_next;
+	int      flag_a;
+	int      flag_b;
 
 	if ((*a)->next == (*b))
 		swap_1(head, a, b);
@@ -83,26 +110,15 @@ void	do_swap(t_subdir **head, t_subdir **a, t_subdir **b)
 		swap_1(head, b, a);
 	else
 	{
-		flag_a = 0;
-		flag_b = 0;
 		a_prev = *head;
 		b_prev = *head;
-		while (a_prev != (*a) && a_prev && a_prev->next != (*a))
-			a_prev = a_prev->next;
-		while (b_prev != (*b) && b_prev && b_prev->next != (*b))
-			b_prev = b_prev->next;
-		if (a_prev && a_prev == *head && a_prev->next != (*a))
-			flag_a = 1;
-		if (b_prev && b_prev == *head && b_prev->next != (*b))
-			flag_b = 1;
+		prev_initil(a, b, &a_prev, &b_prev);
+		flag_a = a_flag_initil(a_prev, head, a);
+		flag_b = b_flag_initil(b_prev, head, b);
 		a_next = (*a)->next;
-		b_next = (*b)->next;
-		(*a)->next = b_next;
+		(*a)->next = (*b)->next;
 		(*b)->next = a_next;
-		if (flag_a == 0)
-			a_prev->next = (*b);
-		else
-			(*head) = (*b);
+		a_head_condition(flag_a, head, b, &a_prev);
 		if (flag_b == 0)
 			b_prev->next = (*a);
 		else
